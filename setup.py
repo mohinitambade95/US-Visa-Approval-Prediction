25353e29cd28175174201af05a10b28d97455980
W3_darshanamahalle_70513 <darshanamahalle33@gmail.com>
2024-03-02 19:12:59 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='us-visa-predction',
+    version='0.0.0',
+    packages=find_packages(),
+    license='MIT',
+    author='Mohini',
+    author_email='mohinitambade95@gmail.com',   
+   
+)
