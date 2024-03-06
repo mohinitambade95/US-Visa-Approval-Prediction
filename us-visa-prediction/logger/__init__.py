c63a63498eba5f9464a980e7552debb39d047644
Mohini <mohinitambade95@gmail.com>
2024-03-06 08:27:32 +0530

    updated logger,exeception and constants

---
+++ us-visa-prediction/logger/__init__.py
@@ -0,0 +1,19 @@
+import logging
+import os
+
+from from_root  import  from_root
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m/%d/%Y %H:%M_%S')}.log"
+log_dir = "logs"
+os.makedirs(log_dir,exist_ok=True)
+logs_path = os.path.join(from_root(),log_dir, LOG_FILE)
+
+logging.basicConfig(
+    filename=logs_path,
+    level=logging.DEBUG,
+    format="[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s")
+
+
+
+
